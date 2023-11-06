50242edb9976a4fe57dca745a85615bbb9ce686c
travor20814 <travor20814@gmail.com>
2023-11-06 15:35:11 +0800

    fix: write all variables to storybook container root

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,15 +1,131 @@
-import type { Renderer, PartialStoryFn as StoryFunction, StoryContext } from '@storybook/types';
+import type {
+  Renderer,
+  PartialStoryFn as StoryFunction,
+  StoryContext,
+} from '@storybook/types';
 import { useEffect, useGlobals, useMemo } from '@storybook/preview-api';
 import { PALETTE } from './constants';
 
-export const withGlobals = (StoryFn: StoryFunction<Renderer>, context: StoryContext<Renderer>) => {
+export const withGlobals = (
+  StoryFn: StoryFunction<Renderer>,
+  context: StoryContext<Renderer>
+) => {
   const [globals] = useGlobals();
-  const palette = useMemo(() => ({
-    ...Object.entries(PALETTE.primary).reduce((result, [key, value]) => ({
-      ...result,
-      [`primary-${key}`]: { value: globals[value.key], cssVariable: value.variable }
-    }), {} as Record<string, { value: string; cssVariable: string }>),
-  }), [globals]);
+  const palette = useMemo(
+    () => ({
+      ...Object.entries(PALETTE.primary).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`primary-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.secondary).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`secondary-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.error).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`error-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.warning).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`warning-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.success).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`success-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.text).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`text-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.action).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`action-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.background).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`background-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.border).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`border-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.divider).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`divider-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+      ...Object.entries(PALETTE.overlay).reduce(
+        (result, [key, value]) => ({
+          ...result,
+          [`overlay-${key}`]: {
+            value: globals[value.key],
+            cssVariable: value.variable,
+          },
+        }),
+        {} as Record<string, { value: string; cssVariable: string }>
+      ),
+    }),
+    [globals]
+  );
 
   useEffect(() => {
     const selector = '#storybook-root';
